14eaf4fb99ecfc0bfc293930523f324ea6a885a2
hzm121 <2696333036@qq.com>
2020-02-03 22:50:57 +0800

    单元测试完成

--- src/test/java/com/example/demo0/Demo0ApplicationTests.java
+++ src/test/java/com/example/demo0/Demo0ApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.demo0;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.net.MalformedURLException;
+import java.net.URL;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Demo0Application.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Demo0ApplicationTests {
+    @LocalServerPort
+   private int port;
 
+    private URL base;
+    @Autowired
+    private TestRestTemplate template;
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        System.err.println("1212122"+this.base);
+        this.base = new URL("http://localhost:"+port+"/");
+    }
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.err.println(template);
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        Assert.assertThat(response.getBody(), CoreMatchers.equalTo("Hello SpringBoot!"));
     }
 
 }
