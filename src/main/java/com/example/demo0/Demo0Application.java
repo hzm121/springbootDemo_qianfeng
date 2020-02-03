9e934a8ef425864ae66f4f4afdab21a58ff9c8d7
hzm121 <2696333036@qq.com>
2020-02-03 17:38:03 +0800

    启动应用测试

--- src/main/java/com/example/demo0/Demo0Application.java
+++ src/main/java/com/example/demo0/Demo0Application.java
@@ -2,10 +2,17 @@ package com.example.demo0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Demo0Application {
-
+    @RequestMapping(value = "",method = RequestMethod.GET)
+public String sayHello(){
+    return "Hello SpringBoot!";
+}
     public static void main(String[] args) {
         SpringApplication.run(Demo0Application.class, args);
     }
