7ad29c8455adae4b5d58b1a271cae161b32ce5c7
hzm121 <2696333036@qq.com>
2020-02-05 11:36:06 +0800

    完成tk.mybatis的使用

--- src/main/java/com/example/demo0/Demo0Application.java
+++ src/main/java/com/example/demo0/Demo0Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan(basePackages = "com.example.demo0.dao")
 @SpringBootApplication
 public class Demo0Application {
     public static void main(String[] args) {
--- src/test/java/com/example/demo0/Demo0ApplicationTests.java
+++ src/test/java/com/example/demo0/Demo0ApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.demo0;
 
+import com.example.demo0.dao.AuserMapper;
+import com.example.demo0.entity.Auser;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,29 +16,59 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Demo0Application.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = Demo0Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+@Rollback
 class Demo0ApplicationTests {
     @LocalServerPort
-   private int port;
+    private int port;
 
     private URL base;
     @Autowired
     private TestRestTemplate template;
+
     @BeforeEach
     public void setUp() throws MalformedURLException {
-        System.err.println("1212122"+this.base);
-        this.base = new URL("http://localhost:"+port+"/");
+        this.base = new URL("http://localhost:" + port + "/");
     }
+
     @Test
     public void contextLoads() {
         System.err.println(template);
         ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
         Assert.assertThat(response.getBody(), CoreMatchers.equalTo("Hello SpringBoot!"));
+    }
+
+    @Autowired
+    private AuserMapper auserMapper;
+
+    @Test
+    public void testSelect() {
+        List<Auser> ausers = auserMapper.selectAll();
+        for (Auser u : ausers) {
+            System.out.println(u.getName());
+        }
+    }
+
+    @Test
+    public void testPage(){
+        PageHelper.startPage(1,10);
+//        List<Auser> ausers = auserMapper.selectByExample(new Example(Auser.class));
+        List<Auser> ausers = auserMapper.selectAll();
+        PageInfo<Auser> list = new PageInfo<>(ausers);
+        for (Auser u :list.getList()){
+            System.out.println(u.getPwd());
+        }
     }
 
 }
